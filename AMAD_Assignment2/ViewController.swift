6fb78a70e5eada82043140cb915239410a5d028a
lee0392 <pranalee@cpe-2606-a000-668e-a900-0-0-0-3.dyn6.twc.com>
2018-09-10 16:05:22 -0400

    Initial Commit

---
+++ AMAD_Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AMAD_Assignment2
+//
+//  Created by Pranalee Jadhav on 9/10/18.
+//  Copyright © 2018 Pranalee Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
